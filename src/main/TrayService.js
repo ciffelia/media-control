0f569ab79727b4db6187b68192894e9d0d351326
Ciffelia <mc.prince.0203@gmail.com>
2020-01-28 23:54:59 +0900

    Better tray icon on Windows

--- src/main/TrayService.js
+++ src/main/TrayService.js
@@ -6,7 +6,7 @@ class TrayService {
     this.init = this.init.bind(this)
     this._handleExitClick = this._handleExitClick.bind(this)
 
-    this._iconPath = path.join(__dirname, '../../assets/icon.png')
+    this._iconPath = this._getIconPath()
   }
 
   init () {
@@ -17,6 +17,14 @@ class TrayService {
       { label: 'Exit', type: 'normal', click: this._handleExitClick }
     ])
     this._tray.setContextMenu(menu)
+  }
+
+  _getIconPath () {
+    if (process.platform === 'win32') {
+      return path.join(__dirname, '../../assets/icon.ico')
+    } else {
+      return path.join(__dirname, '../../assets/icon.png')
+    }
   }
 
   _handleExitClick () {
